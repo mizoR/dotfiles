88f5dd4c6b53817413f9d1d133d843aa44314d7c
mizokami <r.mizokami@gmail.com>
2017-04-22 06:01:55 +0900

    Use trout theme at fish

--- config/fish/conf.d/config.fish
+++ config/fish/conf.d/config.fish
@@ -5,12 +5,12 @@ if test $status -eq 0
   set -gx PATH $PATH $GOROOT/bin $GOPATH/bin
 end
 
-if test -d .rbenv
+if test -d $HOME/.rbenv
   set -gx PATH $HOME/.rbenv/bin $PATH
   rbenv init - | source
 end
 
-if test -d .plenv
+if test -d $HOME/.plenv
   set -gx PLENV_ROOT $HOME/.plenv
   set -gx PATH $PLENV_ROOT/bin $PATH
   plenv init - | source
