65f48977ecc68a00a9358cae003fd777934f3b23
mizokami <r.mizokami@gmail.com>
2018-02-14 14:56:06 +0900

    bitbar: cal: Make simple

--- bitbar/cal.1d.sh
+++ bitbar/cal.1d.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-year=$(date +%Y)
-next_year=$((year+1))
-
 echo "📅"
 echo "---"
 
-cal "$year" | while IFS= read -r i; do echo ". $i | font=courier"; done
-cal "$next_year" | while IFS= read -r i; do echo ". $i | font=courier"; done
+cal -h -3 | while IFS= read -r i; do echo ". $i | font=courier color=black"; done
