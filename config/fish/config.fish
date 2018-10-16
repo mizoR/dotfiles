53d47b5327881421776b84ad7c95bebe21c21611
mizokami <r.mizokami@gmail.com>
2018-10-16 17:21:23 +0900

    Add path for LLVM

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,4 @@
 set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin"    $fish_user_paths
+set -gx LDFLAGS  "-L/usr/local/opt/llvm/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
