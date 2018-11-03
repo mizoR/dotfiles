14ca077a0f3e844a5e4993cbfcb0f94176220c6f
mizokami <r.mizokami@gmail.com>
2018-11-03 14:42:27 +0900

    Check llvm dir before set environment variables for LLVM

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,7 @@
 set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/llvm/bin"    $fish_user_paths
-set -gx LDFLAGS  "-L/usr/local/opt/llvm/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
+
+if test -d /usr/local/opt/llvm
+  set -g fish_user_paths "/usr/local/opt/llvm/bin"    $fish_user_paths
+  set -gx LDFLAGS  "-L/usr/local/opt/llvm/lib"
+  set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
+end
